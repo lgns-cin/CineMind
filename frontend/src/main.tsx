50ea761f6158b3ca17b4e0a6386b65ef22327046
Luan Gustavo <lgns@cin.ufpe.br>
2025-11-03 14:00:36 -0300

    feat: atualiza estrutura do `root`

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import CineMind from "./CineMind.tsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CineMind />
+    <BrowserRouter>
+      <CineMind />
+    </BrowserRouter>
   </StrictMode>
 );
