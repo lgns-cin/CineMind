382e313161fccff582c2213af608e60f065f7362
Luan Gustavo <lgns@cin.ufpe.br>
2025-11-04 15:27:53 -0300

    feat(front): cria componente para ícones da página de login

---
+++ frontend/src/assets/ClosedLockIcon.tsx
@@ -0,0 +1,21 @@
+import SVGIcon, { type IconProps } from "./SVGIcon";
+
+export default function ClosedLockIcon({
+  className = "",
+  viewBox = "0 0 24 24",
+  onClick = _ => {}
+}: IconProps) {
+  return (
+    <SVGIcon
+      className={className}
+      viewBox={viewBox}
+      onClick={onClick}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+      />
+    </SVGIcon>
+  );
+}
