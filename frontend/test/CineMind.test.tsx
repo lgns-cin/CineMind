8fe503ab6e1d6e96dd685c4863c7e1650bc4d372
Luan Gustavo <lgns@cin.ufpe.br>
2025-11-25 14:20:57 -0300

    test(front): amplia teste de navegação de rotas do componente `CineMind`

--- frontend/test/CineMind.test.tsx
+++ frontend/test/CineMind.test.tsx
@@ -13,6 +13,13 @@ describe("Componente CineMind", () => {
     ["/profile", "profile-page"]
   ]);
 
+  /** Nomes das rotas que carregam diretamente na página (sem necessidade de autenticação) */
+  const exposedRoutes = ["/", "/login", "/signup"];
+
+  /** Nomes das rotas que requerem tokens de autenticação */
+  const protectedRoutes = ["/questionnaire" /*, "/home", "/profile" */];
+  // Devemos tirar as outras strings do comentário à medida que adicionamos as páginas
+
   test("Navegação das rotas", async () => {
     for (const [route, testID] of routesToTestIDs.entries()) {
       render(
@@ -21,7 +28,17 @@ describe("Componente CineMind", () => {
         </MemoryRouter>
       );
 
-      expect(await screen.findByTestId(testID)).toBeInTheDocument();
+      if (exposedRoutes.includes(route)) {
+        // A página pode ser visitada
+        // O ID de teste deve ser o testID
+        expect(await screen.findByTestId(testID)).toBeInTheDocument();
+      }
+
+      if (protectedRoutes.includes(route)) {
+        // Você deve ser redirecionado a outra página
+        // O ID de teste não deve ser o testID
+        expect(screen.queryByTestId(testID)).toBeFalsy();
+      }
     }
   });
 });
