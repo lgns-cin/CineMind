b3d731fda3b6ac409927da9c5d6e07e9fdf4d423
Luan Gustavo <lgns@cin.ufpe.br>
2025-12-09 18:17:13 -0300

    fix(front): hotfix erros de build devido ao `EnvironmentPlugin`

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import EnvironmentPlugin from "vite-plugin-environment";
 
+// Só use o plugin EnvironmentPlugin em ambiente de testes...
+let arr;
+if (process.env.NODE_ENV == "production") arr = [tailwindcss(), react()];
+else arr = [tailwindcss(), react(), EnvironmentPlugin("all")];
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), EnvironmentPlugin("all")]
+  plugins: arr
 });
